a4dc9080f32b8bcdc7b132364911a2e630caf8b0
hecong <hecong@cac.co.jp>
2023-03-16 20:27:28 +0900

    fix version

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 // import os module
 const os = require("os");
 
 // get host name
 const hostName = os.hostname();
-
-app.get('/', (req, res) => {
-  console.log(`${(new Date())} ${hostName} : access / v1`);
-  res.send(`${(new Date())} Hello World! from  ${hostName} v1`)
-})
+const version = "v" + 1;
+app.get("/", (req, res) => {
+  console.log(`${new Date()} ${hostName} : access / ` + version);
+  res.send(`${new Date()} Hello World! from  ${hostName} ` + version);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
